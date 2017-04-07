187e4700e997c8800e73e28c2911131c06269515
John Mettraux <jmettraux@gmail.com>
2017-04-07 17:55:45 +0900

    Change range behaviour

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@
 fetch:
 	bundle exec ruby lib/brutespot.rb -until 2015
 
+# `RANGE=201702 make mail`
 mail:
 	bundle exec ruby lib/brutemail.rb
 
--- lib/brutemail.rb
+++ lib/brutemail.rb
@@ -5,7 +5,7 @@ fail ArgumentError.new("missing RANGE") unless range
 uri = File.read('.uri').strip
 email = File.read('.ril').strip
 
-if range.length < 5
+if range.length < 5 # months of a year
   (1..12).each do |m|
     sm = "%02i" % m
     next if Dir["out/#{range}#{sm}*.txt"].empty?
@@ -13,7 +13,7 @@ if range.length < 5
     puts cmd
     system cmd
   end
-else
+else # month
   cmd = "cat out/#{range}*.txt | mutt -s \"#{uri} #{range}\" #{email}"
   puts cmd
   system cmd
